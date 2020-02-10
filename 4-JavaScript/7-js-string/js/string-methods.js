c2705447e0ec0efb6671d5fc10dc93bf4cc2940b
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-02-10 16:25:28 +0100

    string methods

--- 4-JavaScript/7-js-string/js/string-methods.js
+++ 4-JavaScript/7-js-string/js/string-methods.js
@@ -22,4 +22,26 @@ function indexesFinder(txt, chr, start) {
     }
 }
 
+console.log("/////////////////////")
+function textAdd(num){
+if (num == 0){
+    return num
+} else {
+return    textAdd(num-1) + ',' + num  
+}
+}
+
+console.log(textAdd(9))
+console.log("/////////////////////")
+
+
+let arrowTextAdd = num => {
+    if (num == 0) {
+        return num
+    } else {
+        return arrowTextAdd(num-1) + ',' + num
+    }
+}
+
+console.log(arrowTextAdd(3))
 
