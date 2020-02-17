9cd64f5cf958fb817442b1abd6386703b3a12a44
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-02-17 16:33:58 +0100

    array start

--- 4-JavaScript/9-js-for-loop/js/index1.js
+++ 4-JavaScript/9-js-for-loop/js/index1.js
@@ -73,14 +73,40 @@
     
 // }
 
-for (let i = 4; i >= 0; i--) {
-    for (let x = 0; x <= 4; x++) {
-        if( i <= x){
-            document.write("*")
-        } else {
-            document.write("&nbsp; ")
-        }
-    }
-    document.write("<br>")
+//     *
+//    **
+//   ***
+//  ****
+// *****
+// for (let i = 4; i >= 0; i--) {
+//     for (let x = 0; x <= 4; x++) {
+//         if( i <= x){
+//             document.write("*")
+//         } else {
+//             document.write("&nbsp; ")
+//         }
+//     }
+//     document.write("<br>")
     
-}
+// }
+
+function showBox(num) {
+    for (let i = 0; i < num; i++) {
+        for (let x = 0; x < num; x++) {
+            if(
+                i == 0 ||
+                 x == 0 ||
+                  x == num - 1 ||
+                   i == num - 1 ||
+                   i == x ||
+                   i + x == num - 1
+                   ){
+                document.write('*')
+            } else {
+                document.write('&nbsp; ')
+            }
+        }
+        document.write('<br>')
+    }
+  }
+  showBox(10)
