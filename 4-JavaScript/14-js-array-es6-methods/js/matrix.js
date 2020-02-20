c5d2bd2caaca20d86a12f2d2fd1269daa49ee2d2
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-02-20 16:26:53 +0100

    array exercises

--- 4-JavaScript/14-js-array-es6-methods/js/matrix.js
+++ 4-JavaScript/14-js-array-es6-methods/js/matrix.js
@@ -8,3 +8,68 @@ let matrix = [
     [99,-44,67,7,12,6,34,45,55,55],
     [12,6,56,7,23,6,22,4,55,554]
 ]
+function printNewLine(arr) {
+    arr.forEach(item => {
+        item.forEach(subItem => {
+            document.write(subItem + '<br>')
+        });
+    });
+}
+
+//printNewLine(matrix)
+
+function printSum(arr) {
+    let mainStor = 0
+    arr.forEach(item => {
+        let stor = 0
+        item.forEach(subItem => {
+            stor += subItem
+        })
+        mainStor += stor
+        document.write(stor + '<br>')
+    })
+    document.write(mainStor + '<br>')
+  }
+  //printSum(matrix)
+
+  function findMax(arr){
+      let max = arr[0][0]
+      arr.forEach(item => {
+        item.forEach(subItem => {
+            if(max < subItem) {
+                max = subItem
+            }
+        })
+      })
+      return max
+  }
+  //document.write(findMax(matrix) + '<br>')
+
+  function findMin(arr){
+    let min = arr[0][0]
+    arr.forEach(item => {
+      item.forEach(subItem => {
+          if(min > subItem) {
+              min = subItem
+          }
+      })
+    })
+    return min
+}
+//document.write(findMin(matrix) + '<br>')
+
+let treeArray = ['ahmad',30,['omar', 5,['Ali',2,['fbw5',20,[['hi', 50, []]]]]],['blabla',99,[]]]
+
+function recursivePrint(arr){
+    arr.forEach(item => {
+        if(typeof(item) == 'string'){
+            document.write(item + '<br>')
+        } else {
+            if(typeof(item) == 'object'){
+                recursivePrint(item)
+            }
+        }
+    })
+}
+
+recursivePrint(treeArray)
