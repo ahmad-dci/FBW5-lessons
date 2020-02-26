325a37784e8b2581c1ac43dd661a91cb7734eacb
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-02-26 11:25:58 +0100

    classes 2

--- 4-JavaScript/19-oop-intro/js/index3.js
+++ 4-JavaScript/19-oop-intro/js/index3.js
@@ -4,14 +4,47 @@ class Participant {
         this.lastName = blalastName
     }
     fullName() {
-        return this.firstName + ' ' + this.lastName
+        return this.firstName + '-' + this.lastName
       }
 }
 
 
 
 let tim = new Participant('Breuel','Tim')
+tim.tattoos = true
 document.write( tim.fullName() + '<br>')
 
 let safa = new Participant('Bouhlel', 'Safa')
 document.write( safa.fullName() + '<br>')
+
+
+let shirin = new Participant('Valizadeh', 'Shirin')
+document.write( shirin.fullName() + '<br>')
+let belal = new Participant('Sukari', 'Belal')
+
+let parArr = [tim, safa, shirin, belal]
+
+// parArr.forEach(blaItem => {
+//     document.write( blaItem.firstName + '<br>')
+// })
+
+// function to print first name of each element inside parArr
+// first solution 'direct print'
+function firstNamePrinter1(arr) {
+    arr.forEach(item => {
+      document.write(item.firstName + ' '+item.tattoos + '<br>')
+    })
+  }
+
+  firstNamePrinter1(parArr)
+
+  // second solution 'return a storage'
+  function firstNamePrinter2(arr) {
+      let storage = ''
+        arr.forEach(item => {
+             storage += item.firstName + '<br>'
+        })
+        return storage
+      }
+
+      document.write(firstNamePrinter2(parArr))
