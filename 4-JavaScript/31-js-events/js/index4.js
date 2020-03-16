1a30464a195d40ed72ced35d58b79ab878f62813
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-03-16 16:33:46 +0100

    homework

--- 4-JavaScript/31-js-events/js/index4.js
+++ 4-JavaScript/31-js-events/js/index4.js
@@ -4,11 +4,10 @@ window.onload = ()=>{
         let ballDiv = document.createElement('div')
         ballDiv.classList.add('ball')
         //ballDiv.setAttribute('class', 'ball')
-        console.log(e)
         ballDiv.style.left = e.clientX -5  + 'px'
         ballDiv.style.top = e.clientY -5  + 'px'
         container.append(ballDiv)
-        throwTheBall(ballDiv,e.clientY -5, e.clientX -5, 500, 10  )
+        realThrowTheBall(ballDiv,e.clientY -5, e.clientX -5, 500, 10  )
         //alert(e.clientX + '  ' + e.clientY)
 
     })
@@ -31,6 +30,31 @@ function throwTheBall(ball, y, x, parentHight, ballHeight) {
         }
         if(counter == 0){
             down = true
+        }
+    }, 1);
+  }
+
+  function realThrowTheBall(ball, y, x, parentHight, ballHeight) {
+    ball.style.left = x
+    let down = true
+    let counter = y
+    let maxHeight = y
+    let inerv = setInterval(() => {
+        ball.style.top = counter+ 'px';
+        if(down){
+            counter++;
+        } else {
+            counter--;
+        }
+        if(counter == parentHight - ballHeight){
+            down = false
+            maxHeight += 20
+        }
+        if(counter == maxHeight ){
+            down = true
+        }
+        if(parentHight - ballHeight < maxHeight || counter < maxHeight){
+            clearInterval(inerv)
         }
     }, 1);
   }
