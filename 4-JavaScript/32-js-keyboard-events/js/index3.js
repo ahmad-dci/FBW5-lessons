a964788f399f1142365adf8ed6b6211b9ad71b30
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-03-18 15:56:59 +0100

    invoice system7

--- 4-JavaScript/32-js-keyboard-events/js/index3.js
+++ 4-JavaScript/32-js-keyboard-events/js/index3.js
@@ -43,73 +43,7 @@ window.onload = () => {
       let newProduct = document.querySelector('#newProduct')
       newProduct.addEventListener('click', function (e) {
           e.preventDefault()
-          let productDiv = document.createElement('div')
-          productDiv.classList.add('product')
-
-          let firstDiv = document.createElement('div')
-          let newproductInput = document.createElement('input')
-          newproductInput.type = 'text'
-          firstDiv.append(newproductInput)
-          productDiv.append(firstDiv)
-
-          let secondDiv = document.createElement('div')
-          let newPriceInput = document.createElement('input')
-          newPriceInput.type = 'number'
-          newPriceInput.setAttribute('min', '0')
-          newPriceInput.value = '0'
-          secondDiv.append(newPriceInput)
-          productDiv.append(secondDiv)
-
-          let thirdDiv = document.createElement('div')
-          let newquantityInput = document.createElement('input')
-          newquantityInput.type = 'number'
-          newquantityInput.setAttribute('min', '0')
-          newquantityInput.value = '0'
-          thirdDiv.append(newquantityInput)
-          productDiv.append(thirdDiv)
-
-          let fourthDiv = document.createElement('div')
-          let newResultSpan = document.createElement('span')
-          newResultSpan.innerText = '0'
-          fourthDiv.append(newResultSpan)
-          productDiv.append(fourthDiv)
-
-          let productsDiv = document.querySelector('.products')
-          productsDiv.append(productDiv)
-
-          setEvent('keyup',newPriceInput, newPriceInput, newquantityInput, newResultSpan,'price')
-          setEvent('change',newPriceInput, newPriceInput, newquantityInput, newResultSpan,'price')
-          setEvent('keyup',newquantityInput, newPriceInput, newquantityInput, newResultSpan, 'quantity')
-          setEvent('change',newquantityInput, newPriceInput, newquantityInput, newResultSpan,'quantity')
-          // here you need to write your events handler
-
-          newproductInput.addEventListener('keyup', function (e) {
-            if(e.which == 13){
-              newPriceInput.focus()
-            }
-            })
-
-    //       productsDiv.innerHTML += '<div class="product">'+
-    //       '<div>'+
-    //           '<input type="text">'+
-    //       '</div>'+
-    //       '<div>'+
-    //           '<input type="number" min="0" value="0">'+
-    //       '</div>'+
-    //       '<div>'+
-    //           '<input type="number" min="0" value="0">'+
-    //       '</div>'+
-    //       '<div>'+
-    //           '<span>0</span>'+
-    //       '</div>'+
-    //   '</div>'
-
-
-
-
-
-
-
+          newProductCreator()
         })
 }
 
@@ -141,6 +75,84 @@ function setEvent(eventName,triggerElement, priceElement, quantityElement, resul
         quantityElement.focus()
       }
     }
+    if(elementType == 'quantity' && eventName == 'keyup' ){
+      if(e.which == 13){
+        //quantityElement.focus()
+        newProductCreator()
+      }
+    }
 
   })
   }
+
+
+  function newProductCreator() {
+    let productDiv = document.createElement('div')
+    productDiv.classList.add('product')
+
+    let firstDiv = document.createElement('div')
+    let newproductInput = document.createElement('input')
+    newproductInput.type = 'text'
+    firstDiv.append(newproductInput)
+    productDiv.append(firstDiv)
+
+    let secondDiv = document.createElement('div')
+    let newPriceInput = document.createElement('input')
+    newPriceInput.type = 'number'
+    newPriceInput.setAttribute('min', '0')
+    newPriceInput.value = '0'
+    secondDiv.append(newPriceInput)
+    productDiv.append(secondDiv)
+
+    let thirdDiv = document.createElement('div')
+    let newquantityInput = document.createElement('input')
+    newquantityInput.type = 'number'
+    newquantityInput.setAttribute('min', '0')
+    newquantityInput.value = '0'
+    thirdDiv.append(newquantityInput)
+    productDiv.append(thirdDiv)
+
+    let fourthDiv = document.createElement('div')
+    let newResultSpan = document.createElement('span')
+    newResultSpan.innerText = '0 EUR'
+    fourthDiv.append(newResultSpan)
+    productDiv.append(fourthDiv)
+
+    let productsDiv = document.querySelector('.products')
+    productsDiv.append(productDiv)
+
+    setEvent('keyup',newPriceInput, newPriceInput, newquantityInput, newResultSpan,'price')
+    setEvent('change',newPriceInput, newPriceInput, newquantityInput, newResultSpan,'price')
+    setEvent('keyup',newquantityInput, newPriceInput, newquantityInput, newResultSpan, 'quantity')
+    setEvent('change',newquantityInput, newPriceInput, newquantityInput, newResultSpan,'quantity')
+    // here you need to write your events handler
+
+    newproductInput.addEventListener('keyup', function (e) {
+      if(e.which == 13){
+        newPriceInput.focus()
+      }
+      })
+      newproductInput.focus()
+//       productsDiv.innerHTML += '<div class="product">'+
+//       '<div>'+
+//           '<input type="text">'+
+//       '</div>'+
+//       '<div>'+
+//           '<input type="number" min="0" value="0">'+
+//       '</div>'+
+//       '<div>'+
+//           '<input type="number" min="0" value="0">'+
+//       '</div>'+
+//       '<div>'+
+//           '<span>0</span>'+
+//       '</div>'+
+//   '</div>'
+
+
+
+
+
+
+
+
+    }
