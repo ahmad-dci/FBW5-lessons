0f01d37aabf854e1f2fe5932b503a00246119f00
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-03-31 16:46:57 +0200

    students management

--- 4-JavaScript/41-js-localstorage-exercise/js/index.js
+++ 4-JavaScript/41-js-localstorage-exercise/js/index.js
@@ -6,6 +6,7 @@ window.onload = function(){
     let gradeListElement = document.querySelector('#gradeListElement')
     let saveStudentBtn = document.querySelector('#saveStudentBtn')
     let studentNameInp = document.querySelector('#studentNameInp')
+    let body = document.querySelector('body')
 
     // declare empty array to save grade OBJECTS
     let gradesArr = []
@@ -56,6 +57,41 @@ window.onload = function(){
           }
           
         })
+
+        // my code is here to bring data from local storage and render it
+        let storageData = localStorage.getItem('students')
+        console.log(storageData);
+        if(storageData){
+            let dataObj = JSON.parse(storageData)
+            console.log(dataObj);
+            // create container div and add it to the body
+            let containerDiv = document.createElement('div')
+            body.append(containerDiv)
+            dataObj.forEach(element => {
+                console.log(element);
+                let nameH3 = document.createElement('h3')
+                nameH3.innerText = element.name
+                containerDiv.append(nameH3)
+                element.grades.forEach(grade => {
+                    console.log(Object.keys(grade));
+                    // create ul for the grades and append it to containerDiv
+                    let gradeUl = document.createElement('ul')
+                    containerDiv.append(gradeUl)
+                    Object.keys(grade).forEach(key =>{
+                        console.log(key);
+                        console.log(grade[key]);
+                        let gradeLi = document.createElement('li')
+                        gradeLi.innerText = key + ": " + grade[key] + "%"
+                        gradeUl.append(gradeLi)
+                        
+                    })
+                    
+                })
+            });
+            
+        }
+        
+        
 
 }
 
