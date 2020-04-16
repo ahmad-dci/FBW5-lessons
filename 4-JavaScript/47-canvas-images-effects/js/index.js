14ffe84e27f8ec8b1772e0cccbbc80a432c280ae
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-04-16 12:35:07 +0200

    remove red canvas

--- 4-JavaScript/47-canvas-images-effects/js/index.js
+++ 4-JavaScript/47-canvas-images-effects/js/index.js
@@ -6,6 +6,10 @@
 window.onload = () => {
     // get html elements
     const canv = document.querySelector('#canv')
+    const grayScalBtn = document.querySelector('#grayscaleBtn')
+    const orginalBtn = document.querySelector('#orginalBtn')
+    const invertBtn = document.querySelector('#invertBtn')
+    const removeRedBtn = document.querySelector('#removeRedBtn')
 
     // create context instance from canv
     const ctx = canv.getContext('2d')
@@ -18,21 +22,68 @@ window.onload = () => {
     img.onload = () => {
         // draw the image
         ctx.drawImage(img, 0, 0, 500, 500)
+        
+    }
+
+    // add event click to grayscal btn
+    grayScalBtn.onclick = () => {
+
         // getting image data (pixels)
         const imgData = ctx.getImageData(0, 0, 500, 500)
-        // console.log(imgData[4])
-        // console.log(imgData[5])
-        // console.log(imgData[6])
-        // console.log(imgData[7])
+        //the second pixel data
+        // console.log(imgData.data[4]) // red
+        // console.log(imgData.data[5])// green
+        // console.log(imgData.data[6]) // blue
+        // console.log(imgData.data[7]) // Alpha (transparancy)
         
-        for (let i = 0; i < imgData.data.length; i+=4) {
+        for (let i = 0; i < imgData.data.length; i+= 4) {
             let avg = (imgData.data[i] + imgData.data[i+1] + imgData.data[i+2]) / 3
             imgData.data[i] = avg; // red
             imgData.data[i+1] = avg; // green
             imgData.data[i+2] = avg; // blue
         }
+        // clear drawing area
+        ctx.clearRect(0, 0, 500, 500)
         // redraw the changed data 
         ctx.putImageData(imgData, 0, 0)
+
+    }
+
+    // add event click to orginal button
+    orginalBtn.onclick = () => {
+        // clear photo first
+        ctx.clearRect(0, 0, 500, 500)
+        ctx.drawImage(img, 0, 0, 500, 500)
+    }
+    // add click event to invert button
+    invertBtn.onclick = () => {
+        // getimage data
+        const imageData = ctx.getImageData(0, 0, 500, 500)
+        // loop through image data array
+        for(let i = 0 ; i < imageData.data.length ; i += 4 ){
+            imageData.data[i] = 255 - imageData.data[i] // red
+            imageData.data[i+1] = 255 - imageData.data[i+1] // green
+            imageData.data[i+2] = 255 - imageData.data[i+2] // blue
+        }
+        // clear drawing area
+        ctx.clearRect(0, 0, 500, 500)
+        // draw the new image
+        ctx.putImageData(imageData, 0, 0)
+    }
+    // add click event to remove red button
+    removeRedBtn.onclick = () => {
+        const imageData = ctx.getImageData(0, 0, 500, 500)
+        // loop through data array
+        for (let i = 0; i < imageData.data.length; i+=4) {
+            if (imageData.data[i] > imageData.data[i+1] && imageData.data[i] > imageData.data[i+2]){
+                imageData.data[i+3] = 0
+            }
+            
+        }
+        // clear old image
+        ctx.clearRect(0, 0, 500, 500)
+        // draw the new image
+        ctx.putImageData(imageData, 0, 0)
     }
 }
 
