24b3f5c106cd99087fe90fc5f7cfe917420d4d67
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-04-30 15:14:19 +0200

    google map

---
+++ 4-JavaScript/48-js-google-map/js/index.js
@@ -0,0 +1,7 @@
+var map;
+    function fbw5Map() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 53.550270, lng: 10.025270},
+        zoom: 15
+      });
+    }
