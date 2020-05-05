0b99f1908dc6e6754bf5eb587d319da7bf7705b4
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-05-05 10:32:29 +0200

    jquery animation2

--- 5-Jquery/2-jquery-animations/js/index.js
+++ 5-Jquery/2-jquery-animations/js/index.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     // option the styles that will be changed
     // time by mileseconds
     // callback is a function will run after finishing the animation
+    // click event btn
     $("#btn").click(function (e) { 
         e.preventDefault();
         $("#someDiv").animate({
@@ -14,10 +15,16 @@ $(document).ready(function () {
             alert('done');
           })
     });
+
+    // mousemoveevent for someDiv
     $('#someDiv').mousemove(function (e) { 
         // values: e.clientX, e.clientY, e.pageX, e.pageY
         // $('#someDiv').stop()
         $(this).stop()
     });
+
+    $('.container > div:nth-child(1)').animate({
+        'left': '350px'
+    }, 2000)
 
 });
