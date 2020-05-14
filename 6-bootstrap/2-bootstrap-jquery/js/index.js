1b54ba96dadff44a204c689b7cceb246a37f2a9d
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-05-14 12:30:24 +0200

    bootstrap exercise

--- 6-bootstrap/2-bootstrap-jquery/js/index.js
+++ 6-bootstrap/2-bootstrap-jquery/js/index.js
@@ -1,9 +1,19 @@
 $(function () {
     $('#loginBtn').click(function (e) { 
         e.preventDefault();
+        
+        // check username inpute is not empty
         let userName = $('#usernameInput').val();
         if (!userName.trim()) {
             $('#usernameAlert').removeClass('d-none');
         }
+
+        // check password inpute is not empty
+        let password = $('#passwordInput').val();
+        if (!password.trim()) {
+            $('#passwordAlert').removeClass('d-none');
+        }
+
+
     });
 });
