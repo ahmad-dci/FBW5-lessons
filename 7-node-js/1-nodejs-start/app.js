ab5457d75067fefa439261163f0ed6649951f715
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-05-26 15:07:22 +0200

    node js rout basics

--- 7-node-js/1-nodejs-start/app.js
+++ 7-node-js/1-nodejs-start/app.js
@@ -1,16 +1,48 @@
 const http = require('http')
+const fs = require('fs')
+
 
 http.createServer((req, res) => {
     // res.writeHead(200, {'content-type': 'application/json'})
-     //res.writeHead(200, {'content-type': 'text/plain'}) // send text data
+    // res.writeHead(200, {'content-type': 'text/plain'}) // send text data
      res.writeHead(200, {'content-type': 'text/html'}) // send html
-    let obj = {
-        name: "ahmad",
-        firstName: "osman"
-    }
+
+     //send json text
+    // let obj = {
+    //     name: "ahmad",
+    //     firstName: "osman"
+    // }
 
     //res.end(JSON.stringify(obj))
-    res.end("<h2>text</h2>");
+
+    //send html text
+    // let myHtml =
+    //  '<html>'+
+    // '<head>'+
+    // '<title>my first server page</title>'+
+    // '</head>'+
+    // '<body>'+
+    // '<h2>text</h2>'+
+    // '</body>'+
+    // '</html>';
+
+    // res.end(myHtml);
+
+
+    //send html text from file
+
+    // reading file
+    // fs.readFile('views/index.html', (err, data) => {
+    //     //console.log(err);
+    //     res.end(data);
+    // })
+
+    console.log(req.url);
+    
+    let text = fs.readFileSync('views/index.html');
+    res.end(text);
+    
+
 }).listen(4000)
 
 
