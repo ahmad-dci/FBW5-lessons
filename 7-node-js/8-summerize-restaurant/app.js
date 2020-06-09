69c877749aa3e55f11984b118965df0cc270f732
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-06-09 09:34:19 +0200

    restaurant

--- 7-node-js/8-summerize-restaurant/app.js
+++ 7-node-js/8-summerize-restaurant/app.js
@@ -19,21 +19,69 @@ app.get('/', (req, res) => {
 // app.get('/home/subhome', (req, res) => {
 //     res.render('main');
 // });
+
+app.get('/admin/addmeal', (req, res) => {
+    res.render('adminAddMeal', {meals: meals})
+});
+app.post('/admin/addmeal', (req, res) => {
+    const mealTitle = req.body.mealTitle
+    const mealPrice = req.body.mealPrice
+    const mealDescription = req.body.mealDescription
+
+    let obj = {
+        title: mealTitle,
+        description: mealDescription,
+        imgUrl: "/img/burger/4.png",
+        price: mealPrice
+    }
+    meals.push(obj)
+    //res.render('adminAddMeal', {meals: meals})
+    res.redirect('/admin/addmeal')
+
+});
+
 app.get('/menu', (req, res) => {
-    res.render('menu')
+    res.render('menu', {meals: meals})
 });
 app.get('/contact', (req, res) => {
-    res.render('contact')
+    res.render('contact', {sent: 1})
 });
 app.post('/contact', (req, res) => {
     console.log(req.body);
-    emailSender.sendEmail(req.body.name, req.body.email, req.body.subject, req.body.message, (ok) => {
-        if(ok){
-            res.sendStatus(200);
-        } else{
-            res.sendStatus(500);
-        }
-    });
+    const name = req.body.name
+    const email = req.body.email
+    const subject = req.body.subject
+    const message = req.body.message
+    if(name != "" && name.length < 100 ){
+        emailSender.sendEmail(name, email, subject, message, (ok) => {
+            if(ok){
+                res.sendStatus(200);
+            } else{
+                res.sendStatus(500);
+            }
+        });
+    }
+    
+   
+});
+app.post('/contact1', (req, res) => {
+    console.log(req.body);
+    const name = req.body.name
+    const email = req.body.email
+    const subject = req.body.subject
+    const message = req.body.message
+    if(name != "" && name.length < 100 ){
+        emailSender.sendEmail(name, email, subject, message, (ok) => {
+            if(ok){
+                //res.sendStatus(200);
+                res.render('contact', {sent: 2})
+            } else{
+                //res.sendStatus(500);
+                res.render('contact', {sent: 3})
+            }
+        });
+    }
+    
    
 });
 
