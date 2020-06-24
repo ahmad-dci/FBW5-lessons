69cc6453dced0c4ac166e74a20687712d141b845
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-06-24 11:54:38 +0200

    books store add book server side start

--- 7-node-js/10-books-store/routes/adminRoutes.js
+++ 7-node-js/10-books-store/routes/adminRoutes.js
@@ -5,5 +5,10 @@ const adminRouter = express.Router()
 adminRouter.get('/addbook', (req, res) => {
     res.render('addbook');
 })
+adminRouter.post('/addbook', (req, res) => {
+console.log(req.body);
+console.log(req.files);
 
+
+})
 module.exports = adminRouter
