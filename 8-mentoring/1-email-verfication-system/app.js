9c6755bde1034a3fe5e8790c2b4b51f05579f161
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-07-17 12:38:41 +0200

    mentoring email verfication

--- 8-mentoring/1-email-verfication-system/app.js
+++ 8-mentoring/1-email-verfication-system/app.js
@@ -1,10 +1,31 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+app.get('/register', (req, res) => {
+    res.render('register')
+});
+app.post('/register', (req, res) => {
+    console.log(req.body);
+    res.json(1)
+});
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
+
+// register page fiels
+// first name
+// last name
+// password
+// repassword
+// email
+// 
