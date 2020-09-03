8dc407b98391312dac9725a360d7c832843eddb2
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-09-03 14:20:31 +0200

    react image search 3

--- 9-react/12-react-events/components/SingleImage.js
+++ 9-react/12-react-events/components/SingleImage.js
@@ -5,15 +5,19 @@ class SingleImage extends React.Component{
         super(props)
         this.overallDivRef = React.createRef()
     }
+    onOverallClick = () => {
+        this.overallDivRef.current.classList.remove('active')
+    }
     onthumbImageClick = () => {
         this.overallDivRef.current.classList.add('active')
+        //this.overallDivRef.current.addEventListener('click', this.onOverallClick)
     }
     render(){
         return (
             <div >
                 <img onClick={this.onthumbImageClick} src={this.props.imageData.previewURL} />
-                <div ref={this.overallDivRef} className="ui  dimmer">
-                    <img  src={this.props.imageData.largeImageURL} />
+                <div onClick={this.onOverallClick} ref={this.overallDivRef} className="ui  dimmer">
+                    <img onClick={(e) => {e.stopPropagation()}}  src={this.props.imageData.largeImageURL} />
                 </div>
             </div>
         )
