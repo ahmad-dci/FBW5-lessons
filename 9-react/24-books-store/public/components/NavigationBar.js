7c518b92c672627f24ebe1d8dc4a1f1fd2b6d231
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-09-16 13:58:46 +0200

    react fullstack navbar reactstrap

--- 9-react/24-books-store/public/components/NavigationBar.js
+++ 9-react/24-books-store/public/components/NavigationBar.js
@@ -1,45 +1,67 @@
 import React from 'react'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+  } from 'reactstrap'
 
 class NavigationBar extends React.Component {
+    state = {
+        isOpen: false
+    }
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
   render() {
     return (
       <header>
           <div className="main-menu">
             <div className="container">
-                <nav className="navbar navbar-expand-lg navbar-light">
-                    <a className="navbar-brand" href="index.html"><img src="/images/logo.png" alt="logo" /></a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ml-auto">
-                            <li className="navbar-item active">
-                                <a href="index.html" className="nav-link">Home</a>
-                            </li>
-                            <li className="navbar-item">
-                                <a href="shop.html" className="nav-link">Shop</a>
-                            </li>
-                            <li className="navbar-item">
-                                <a href="about.html" className="nav-link">About</a>
-                            </li>
-                            <li className="navbar-item">
-                                <a href="faq.html" className="nav-link">FAQ</a>
-                            </li>
-                            <li className="navbar-item">
-                                <a href="login.html" className="nav-link">Login</a>
-                            </li>
-                        </ul>
-                        <div className="cart my-2 my-lg-0">
+            <Navbar color="light" light expand="lg">
+                <NavbarBrand href="/">
+                    <img src="/images/logo.png" alt="logo" />
+                </NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem className="navbar-item" active>
+                            <NavLink href="/components/">Home</NavLink>
+                        </NavItem>
+                        <NavItem className="navbar-item">
+                            <NavLink href="/components/">Shop</NavLink>
+                        </NavItem>
+                        <NavItem className="navbar-item">
+                            <NavLink href="/components/">About</NavLink>
+                        </NavItem>
+                        <NavItem className="navbar-item">
+                            <NavLink href="/components/">FAQ</NavLink>
+                        </NavItem>
+                        <NavItem className="navbar-item">
+                            <NavLink href="/components/">Login</NavLink>
+                        </NavItem>
+                    </Nav>
+                    <div className="cart my-2 my-lg-0">
                             <span>
                                 <i className="fa fa-shopping-cart" aria-hidden="true"></i></span>
                             <span className="quntity">3</span>
-                        </div>
+                    </div>
                         <form className="form-inline my-2 my-lg-0">
                             <input className="form-control mr-sm-2" type="search" placeholder="Search here..." aria-label="Search" />
                             <span className="fa fa-search"></span>
                         </form>
-                    </div>
-                </nav>
+                </Collapse>
+            </Navbar>
             </div>
         </div>
       </header>
