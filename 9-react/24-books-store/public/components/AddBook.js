d621389e242d34fce46f4fb75eb9e5398b8ac840
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2020-09-22 11:59:46 +0200

    react fullstack add book api post

--- 9-react/24-books-store/public/components/AddBook.js
+++ 9-react/24-books-store/public/components/AddBook.js
@@ -37,6 +37,8 @@ const AddBook = () => {
             showModal: true,
             modalTitle: 'Entries Errors'
           })
+        } else {
+          
         }
   }
   const closeModal = () => {
--- 9-react/24-books-store/public/services/api.js
+++ 9-react/24-books-store/public/services/api.js
@@ -1,3 +1,4 @@
+import { resolveContent } from "nodemailer/lib/shared"
 
 export const registerPost = (email, password, repassword) => {
     const sendData = {
@@ -6,20 +7,20 @@ export const registerPost = (email, password, repassword) => {
         repassword
     }
     return new Promise((resolve, reject) => {
-        fetch('/register',{
+        fetch('/register', {
             method: 'POST',
-            headers:{
+            headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(sendData)
         }).then(response => {
-            if(response.status === 200){
+            if (response.status === 200) {
                 response.json().then(recievedData => {
                     resolve(recievedData)
                 }).catch(error => {
                     reject(error)
                 })
-            } else{
+            } else {
                 reject(new Error('can not send data to server. response number is: ' + response.status))
             }
         }).catch(error => {
@@ -41,7 +42,36 @@ export const loginPost = (email, password) => {
             },
             body: JSON.stringify(data)
         }).then(response => {
-            if(response.status === 200) {
+            if (response.status === 200) {
+                response.json().then(data => {
+                    resolve(data)
+                }).catch(error => {
+                    reject(error)
+                })
+            } else {
+                reject(new Error('can not send the data, response number is: ' + response.status))
+            }
+        }).catch(error => {
+            reject(error)
+        })
+    })
+}
+
+export const addBookPost = (bookTitle, bookDescription, bookPdf, bookImgs) => {
+    return new Promise((resolve, reject) => {
+        const fd = new FormData()
+        fd.append('bookTitle', bookTitle)
+        fd.append('bookDescription', bookDescription)
+        for (let i = 0; i < bookImgs.length; i++) {
+            fd.append('bookImg' + i, bookImgs[i])
+        }
+        fd.append('bookPdf', bookPdf)
+
+        fetch('/admin/addbook', {
+            method: 'POST',
+            body: fd
+        }).then(response => {
+            if (response.status === 200) {
                 response.json().then(data => {
                     resolve(data)
                 }).catch(error => {
